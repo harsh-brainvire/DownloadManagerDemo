7d92fd50aee71deff97021c69f634224d68c2dbc
Harsh Dalwadi <harsh.dalwadi@brainvire.com>
2023-08-22 12:39:54 +0530

    open downloaded file based on DownloadId received from receiver

--- app/src/main/java/com/example/downloadmanagerdemo/FirstFragment.kt
+++ app/src/main/java/com/example/downloadmanagerdemo/FirstFragment.kt
@@ -5,6 +5,7 @@ import android.app.DownloadManager.Request.VISIBILITY_VISIBLE
 import android.app.DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED
 import android.content.ActivityNotFoundException
 import android.content.BroadcastReceiver
+import android.content.ContentResolver
 import android.content.Context
 import android.content.Context.DOWNLOAD_SERVICE
 import android.content.Intent
@@ -39,7 +40,7 @@ class FirstFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
@@ -64,7 +65,8 @@ class FirstFragment : Fragment() {
             Log.d(TAG, "onReceive() called with: ctxt = $ctxt, intent = $intent")
             val id = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
 
-            openFile(fileName, id)
+//            openFile(fileName, id)
+            openDownloadedAttachment(requireContext(), id)
         }
     }
 
@@ -137,6 +139,78 @@ class FirstFragment : Fragment() {
 
         dm = requireContext().getSystemService(DOWNLOAD_SERVICE) as DownloadManager?
         dm!!.enqueue(r)
+    }
+
+
+    /**
+     * Used to open the downloaded attachment.
+     *
+     * @param context    Content.
+     * @param downloadId Id of the downloaded file to open.
+     */
+    private fun openDownloadedAttachment(context: Context, downloadId: Long) {
+        val downloadManager = context.getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+        val query = DownloadManager.Query()
+        query.setFilterById(downloadId)
+        val cursor = downloadManager.query(query)
+        if (cursor.moveToFirst()) {
+            val downloadStatus = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
+            val downloadLocalUri =
+                cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI))
+            val downloadMimeType =
+                cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_MEDIA_TYPE))
+            if (downloadStatus == DownloadManager.STATUS_SUCCESSFUL && downloadLocalUri != null) {
+                openDownloadedAttachment(context, Uri.parse(downloadLocalUri), downloadMimeType)
+            }
+        }
+        cursor.close()
+    }
+
+    /**
+     * Used to open the downloaded attachment.
+     *
+     *
+     * 1. Fire intent to open download file using external application.
+     *
+     * 2. Note:
+     * 2.a. We can't share fileUri directly to other application (because we will get FileUriExposedException from Android7.0).
+     * 2.b. Hence we can only share content uri with other application.
+     * 2.c. We must have declared FileProvider in manifest.
+     * 2.c. Refer - https://developer.android.com/reference/android/support/v4/content/FileProvider.html
+     *
+     * @param context            Context.
+     * @param attachmentUri      Uri of the downloaded attachment to be opened.
+     * @param attachmentMimeType MimeType of the downloaded attachment.
+     */
+    private fun openDownloadedAttachment(
+        context: Context,
+        attachmentUri: Uri,
+        attachmentMimeType: String
+    ) {
+        var attachmentUri: Uri? = attachmentUri
+        if (attachmentUri != null) {
+            // Get Content Uri.
+            if (ContentResolver.SCHEME_FILE == attachmentUri.scheme) {
+                // FileUri - Convert it to contentUri.
+                val file = File(attachmentUri.path)
+                attachmentUri =
+                    FileProvider.getUriForFile(
+                        requireContext(), "com.example.downloadmanagerdemo" + ".fileprovider",
+                        file
+                    )
+            }
+            val openAttachmentIntent = Intent(Intent.ACTION_VIEW)
+            openAttachmentIntent.setDataAndType(attachmentUri, attachmentMimeType)
+            openAttachmentIntent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+            try {
+                context.startActivity(openAttachmentIntent)
+            } catch (e: ActivityNotFoundException) {
+                Toast.makeText(
+                    requireContext(), "No handler for this type of file.",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
     }
 
     override fun onDestroyView() {
